fbb15c4996a898d09f8d224c20d9fd938e30f4c3
Niek Palm <dev.npalm@gmail.com>
2017-09-13 21:19:27 +0200

    ecs-cluster

--- main.tf
+++ main.tf
@@ -9,3 +9,37 @@ module "vpc" {
   aws_region  = "${var.aws_region}"
   environment = "${var.environment}"
 }
+
+resource "aws_key_pair" "key" {
+  key_name   = "${var.key_name}"
+  public_key = "${file("${var.ssh_key_file_ecs}")}"
+}
+
+// Bastion module
+module "bastion" {
+  source         = "bastion"
+  enable_bastion = "${var.enable_bastion}"
+
+  aws_region       = "${var.aws_region}"
+  environment      = "${var.environment}"
+  ssh_key_file_pub = "${var.ssh_key_file_bastion}"
+
+  vpc_id    = "${module.vpc.vpc_id}"
+  subnet_id = "${element(module.vpc.public_subnets, 0)}"
+}
+
+// container cluster (ECS)
+module "ecs-cluster" {
+  source = "ecs-cluster"
+
+  aws_region  = "${var.aws_region}"
+  environment = "${var.environment}"
+  key_name    = "${var.key_name}"
+
+  vpc_id   = "${module.vpc.vpc_id}"
+  vpc_cidr = "${module.vpc.vpc_cidr}"
+  subnets  = "${module.vpc.private_subnets}"
+
+  asg_max     = 3
+  asg_desired = 3
+}
--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,3 @@
-
+output "bastion_public_ip" {
+  value = "${module.bastion.public_ip}"
+}
